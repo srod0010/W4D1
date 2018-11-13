801f45b357d5d59f4f2e96bc50f91bac9a2b1f49
srod0010 <saviofrod@gmail.com>
2018-11-12 17:34:17 -0800

    Use update method on database

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :users
-                        
+
+    # resources :users, except: [:new, :edit] do 
+    #
+    #   resources:user
+    # end
+
     get "/users", to: "users#index", as: 'user_id'
     post "/users", to: "users#create"
     get "/users/new", to: "users#new", as: 'new_user'
@@ -9,6 +14,6 @@ Rails.application.routes.draw do
     get "/users/:id", to: "users#show"
     patch "/users/:id", to: "users#update"
     delete "/users/:id", to: "users#destroy"
-               
-                      
+
+
 end
